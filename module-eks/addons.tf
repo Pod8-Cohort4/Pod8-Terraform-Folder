bfc334b0ad857d4ddf3032c321902d55afeaa694
pod8cohort4 <pod8cohort4@gmail.com>
2025-11-27 02:41:47 +0100

    deploy to aws

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -17,6 +17,10 @@ provider "helm" {
 # =========================================
 # EKS AUTH
 # =========================================
+data "aws_eks_cluster" "eks" {
+  name = aws_eks_cluster.eks.name
+}
+
 data "aws_eks_cluster_auth" "cluster" {
   name = aws_eks_cluster.eks.name
 }
