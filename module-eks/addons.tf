d4b38211472a62ea7cfd3a52caa6e093c8cb7c88
pod8cohort4 <pod8cohort4@gmail.com>
2025-11-27 02:30:03 +0100

    Redeploy to aws

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,12 +1,17 @@
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
-  kubernetes ={
-    config_path = "~/.kube/config"
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
+
 
 
 # =========================================
